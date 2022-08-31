0153292b0f10575d847f1ea314c97968ac574ad3
Fabrizio Poli <fabrizio.poli@ajila.com>
2022-08-31 10:49:34 +0200

    language

--- js/language.js
+++ js/language.js
@@ -1,9 +1,14 @@
+$(document).ready(function () {
+    getLanguage();
+    $('#item1').text(language.salutation);
+});
+
 var language;
 
 function getLanguage() {
     (localStorage.getItem('language') == null) ? setLanguage('en') : false;
     $.ajax({
-        url: '/language/' + localStorage.getItem('language') + '.json',
+        url: './language/' + localStorage.getItem('language') + '.json',
         dataType: 'json', async: false, dataType: 'json',
         success: function (lang) { language = lang }
     });
